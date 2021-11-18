e8730cddb3944d18847efbbd4356972618c5271f
Noah McClelland <nmcclelland@trexis.net>
2021-11-18 16:52:54 -0500

    add cursor logic for product summary extension

---
+++ src/main/java/com/backbase/dbs/product/summary/CursorApiRestClientConfiguration.java
@@ -0,0 +1,31 @@
+package com.backbase.dbs.product.summary;
+
+import com.backbase.buildingblocks.communication.client.ApiClientConfig;
+import net.trexis.experts.cursor.cursor_service.api.client.ApiClient;
+import net.trexis.experts.cursor.cursor_service.api.client.v2.CursorApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import static com.backbase.buildingblocks.communication.http.HttpCommunicationConfiguration.INTERCEPTORS_ENABLED_HEADER;
+import static java.lang.Boolean.TRUE;
+
+@Configuration
+public class CursorApiRestClientConfiguration extends ApiClientConfig {
+
+    public static final String SERVICE_ID = "cursor";
+
+    public CursorApiRestClientConfiguration() {
+        super(SERVICE_ID);
+    }
+
+    @Bean
+    public CursorApi cursorApi() {
+        return new CursorApi(createApiClient());
+    }
+
+    private ApiClient createApiClient() {
+        return new ApiClient(getRestTemplate())
+                .setBasePath(createBasePath())
+                .addDefaultHeader(INTERCEPTORS_ENABLED_HEADER, TRUE.toString());
+    }
+}
---
+++ src/main/java/com/backbase/dbs/product/summary/ExtendProductSummaryService.java
@@ -0,0 +1,100 @@
+package com.backbase.dbs.product.summary;
+
+import com.backbase.buildingblocks.backend.security.auth.config.SecurityContextUtil;
+import com.backbase.buildingblocks.presentation.errors.InternalServerErrorException;
+import com.backbase.dbs.product.Configurations;
+import com.backbase.dbs.product.arrangement.ArrangementService;
+import com.backbase.dbs.product.balance.BalanceService;
+import com.backbase.dbs.product.clients.AccessControlClient;
+import com.backbase.dbs.product.clients.JwtContext;
+import com.backbase.dbs.product.repository.ArrangementJpaRepository;
+import lombok.extern.slf4j.Slf4j;
+import net.trexis.experts.cursor.cursor_service.api.client.v2.CursorApi;
+import net.trexis.experts.cursor.cursor_service.v2.model.Cursor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import static java.lang.Thread.sleep;
+import static java.time.LocalDateTime.now;
+import static java.time.LocalDateTime.parse;
+import static net.trexis.experts.cursor.cursor_service.v2.model.Cursor.StatusEnum.IN_PROGRESS;
+import static net.trexis.experts.cursor.cursor_service.v2.model.Cursor.TypeEnum.ENTITY;
+
+@Slf4j
+@Primary
+@Service
+public class ExtendProductSummaryService extends ProductSummaryService {
+
+    private final SecurityContextUtil securityContextUtil;
+    private final CursorApi cursorApi;
+
+    @Value("${arrangement-extension.product-summary.in-progress-cursor.time-wait-seconds:35}")
+    private int inProgressCursorTimeWaitSeconds;
+
+    @Value("${arrangement-extension.product-summary.in-progress-cursor.poll-interval-seconds:2}")
+    private int inProgressCursorPollIntervalSeconds;
+
+
+    public ExtendProductSummaryService(Configurations configurations,
+            ArrangementService arrangementService,
+            JwtContext jwtContext,
+            AccessControlClient accessControlClient,
+            ArrangementJpaRepository arrangementRepository,
+            BalanceService balanceService,
+            SecurityContextUtil securityContextUtil,
+            CursorApi cursorApi) {
+        super(configurations, arrangementService, jwtContext, accessControlClient, arrangementRepository, balanceService);
+        this.securityContextUtil = securityContextUtil;
+        this.cursorApi = cursorApi;
+        log.info("Extended service for cursor-aware read arrangements has been created, configured time wait is {} seconds with a poll interval of {} seconds",
+                inProgressCursorTimeWaitSeconds, inProgressCursorPollIntervalSeconds);
+    }
+
+    @Override
+    public ProductSummary getProductSummary(ProductSummaryFilter filter) {
+        var entityId = securityContextUtil.getUserTokenClaim("member-account-number", String.class)
+                .orElseThrow(() -> new RuntimeException("Unable to identify entityId from token claims while getting product summary. Cannot check cursor status, failing."));
+
+        var entityCursor = getEntityCursor(entityId);
+        var ingestionStartDateTime = parse(entityCursor.getStartDateTime());
+        var productSummaryCallStartDateTime = now();
+
+        if (entityCursor.getStatus() != IN_PROGRESS) {
+            return super.getProductSummary(filter);
+        }
+
+        while (entityCursor.getStatus() == IN_PROGRESS) {
+            log.info("Ingestion is still in progress for entityId {}, started at {}", entityId, ingestionStartDateTime);
+
+            // Regardless if ingestion is still in progress, stop waiting after the configured time
+            if (now().isAfter(ingestionStartDateTime.plusSeconds(inProgressCursorTimeWaitSeconds))) {
+                log.info("Ingestion still in progress, returning existing data as it has surpassed the configured wait time of {} seconds", inProgressCursorTimeWaitSeconds);
+                break;
+            }
+
+            log.info("Sleeping for {} seconds before checking again", inProgressCursorPollIntervalSeconds);
+            try {
+                // Multiply by 1000 for millis instead of seconds
+                sleep(1000L * inProgressCursorPollIntervalSeconds);
+            } catch (InterruptedException e) {
+                log.error("Thread sleep while waiting for in progress cursor to succeed was interrupted! Swallowing exception and allowing while loop to continue.");
+            }
+
+            entityCursor = getEntityCursor(entityId);
+        }
+
+        return super.getProductSummary(filter);
+    }
+
+    private Cursor getEntityCursor(String entityId) {
+        var cursorResponse = cursorApi.getCursorWithHttpInfo(entityId, ENTITY.toString(), null);
+
+        if (!cursorResponse.getStatusCode().is2xxSuccessful()) {
+            log.error("Failed to get ENTITY cursor for entityId {} for getProductSummary. Status code: {}. Returning 500 error.", entityId, cursorResponse.getStatusCode());
+            throw new InternalServerErrorException("Unable to retrieve ENTITY cursor!");
+        }
+
+        return cursorResponse.getBody().getCursor();
+    }
+}
