8f0c889619eb72d79a50160c31c140e26255ab8d
Hendrik Tredoux <hendrik.tredoux@trexis.net>
2022-06-17 18:20:34 -0400

    Jacoco, pipeline, distribution, dependency

---
+++ actions-extension/src/test/java/com/backbase/dbs/actions/extended/handler/TransactionOverThresholdHandlerTest.java
@@ -0,0 +1,7 @@
+package com.backbase.dbs.actions.extended.handler;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TransactionOverThresholdHandlerTest {
+
+}
---
+++ arrangement-extension/src/test/java/com/backbase/dbs/product/summary/ExtendProductSummaryServiceTest.java
@@ -0,0 +1,7 @@
+package com.backbase.dbs.product.summary;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExtendProductSummaryServiceTest {
+
+}
