fc21bcde881fc8a69b6411e27c593eaea9902f9c
Noah McClelland <noah.mcclelland@trexis.net>
2023-03-13 12:48:44 -0400

    add configuration for sorting transactions via an addition. For pending uses createdDateTime; for posted uses bookingDateTime

--- transaction-extension/src/main/java/com/backbase/dbs/transaction/mgmt/presentation/extension/service/ExtendTransactionService.java
+++ transaction-extension/src/main/java/com/backbase/dbs/transaction/mgmt/presentation/extension/service/ExtendTransactionService.java
@@ -14,7 +14,12 @@ import com.backbase.dbs.transaction.mgmt.persistence.repository.SpecificationBui
 import com.backbase.dbs.transaction.mgmt.persistence.repository.TransactionRepository;
 import com.backbase.dbs.transaction.mgmt.persistence.service.TransactionService;
 import com.backbase.dbs.transaction.mgmt.presentation.extension.config.TransactionManagerConfig;
+import com.backbase.transaction.persistence.rest.spec.v2.transactions.TransactionItem;
 import com.backbase.transaction.persistence.rest.spec.v2.transactions.TransactionsGetResponseBody;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import lombok.extern.slf4j.Slf4j;
 import net.trexis.experts.cursor.cursor_service.api.client.v2.CursorApi;
 import net.trexis.experts.cursor.cursor_service.v2.model.Cursor;
@@ -38,15 +43,23 @@ import static net.trexis.experts.cursor.cursor_service.v2.model.Cursor.TypeEnum.
 @Service
 public class ExtendTransactionService extends TransactionService {
 
-    private final TransactionManagerConfig transactionManagerConfig;
+    public static final String DIRECTION_DESCENDING = "DESC";
+    private final TransactionManagerConfig.Ingestion transactionManagerIngestionConfig;
+    private final TransactionManagerConfig.AdditionOrdering transactionManagerAdditionOrderingConfig;
     private final CursorApi cursorApi;
     private final SecurityContextUtil securityContextUtil;
     private final ArrangementsApi arrangementsApi;
 
+    private static final String ORDER_BY_BOOKING_DATE = "bookingDate";
+    private static final String BILLING_STATUS_BILLED = "BILLED";
+    private static final String BILLING_STATUS_UNBILLED = "UNBILLED";
+    private static final String BILLING_STATUS_PENDING = "PENDING";
+
     public ExtendTransactionService(ApplicationContext applicationContext, TransactionRepository transactionRepository, EventBus eventBus, OriginatorContextUtil originatorContextUtil, Validator validator, @Value("${backbase.api.extensions.classes.com.backbase.transaction.persistence.rest.spec.v2.transactions.TransactionsPostRequestBody:null}") String additionalDataConfig, @Value("${backbase.transaction.additions.mode:tableAdditions}") Transaction.AdditionsMode additionsMode, TransacionPostRequestMapper transactionPostRequestMapper, TransactionPatchRequestMapper transactionPatchRequestMapper, SpecificationBuilder specificationBuilder,
                                     TransactionManagerConfig transactionManagerConfig, CursorApi cursorApi, SecurityContextUtil securityContextUtil, ArrangementsApi arrangementsApi) {
         super(applicationContext, transactionRepository, eventBus, originatorContextUtil, validator, additionalDataConfig, additionsMode, transactionPostRequestMapper, transactionPatchRequestMapper, specificationBuilder);
-        this.transactionManagerConfig = transactionManagerConfig;
+        this.transactionManagerIngestionConfig = transactionManagerConfig.getIngestion();
+        this.transactionManagerAdditionOrderingConfig = transactionManagerConfig.getAdditionOrdering();
         this.cursorApi = cursorApi;
         this.securityContextUtil = securityContextUtil;
         this.arrangementsApi = arrangementsApi;
@@ -59,7 +72,7 @@ public class ExtendTransactionService extends TransactionService {
         var response = super.getTransactions(parameterHolder);
 
         // Booking date flip extension
-        if (transactionManagerConfig.isDateFlipEnabled()) {
+        if (transactionManagerIngestionConfig.isDateFlipEnabled()) {
             response.getTransactionItems().stream()
                     .forEach(tx -> {
                         var originalBookingDate = tx.getBookingDate();
@@ -70,7 +83,7 @@ public class ExtendTransactionService extends TransactionService {
 
         // Ingestion extension
         //Only do if enabled
-        if(!transactionManagerConfig.isEnabled()) {
+        if(!transactionManagerIngestionConfig.isEnabled()) {
             log.info("Ingestion cursor progress check disabled");
             return response;
         }
@@ -87,7 +100,7 @@ public class ExtendTransactionService extends TransactionService {
                 try {
                     arrangementCursor = getArrangementCursor(arrangementExternalId);
                 } catch (InternalServerErrorException e) {
-                    if (transactionManagerConfig.isContinueAfterFailedCursorCheck()) {
+                    if (transactionManagerIngestionConfig.isContinueAfterFailedCursorCheck()) {
                         log.warn("Failed to get arrangement cursor: {}", e.getMessage(), e);
                     } else {
                         log.error("Failed to get user cursor, erroring due to configuration continue-after-failed-cursor-check", e);
@@ -102,16 +115,16 @@ public class ExtendTransactionService extends TransactionService {
                     log.info("Ingestion is still in progress for arrangementExternalId {}, started at {}", arrangementExternalId, ingestionStartDateTime);
 
                     // Regardless if ingestion is still in progress, stop waiting after the configured time
-                    if (now().isAfter(ingestionStartDateTime.plusSeconds(transactionManagerConfig.getTimeWaitSeconds()))) {
+                    if (now().isAfter(ingestionStartDateTime.plusSeconds(transactionManagerIngestionConfig.getTimeWaitSeconds()))) {
                         log.info("Reached maximum waiting time of {} seconds for ingestion to complete. Ingestion still in progress, returning existing data",
-                                transactionManagerConfig.getTimeWaitSeconds());
+                                transactionManagerIngestionConfig.getTimeWaitSeconds());
                         break;
                     }
 
-                    log.info("Sleeping for {} seconds before checking again. Maximum waiting time is {}", transactionManagerConfig.getPollIntervalSeconds(), transactionManagerConfig.getTimeWaitSeconds());
+                    log.info("Sleeping for {} seconds before checking again. Maximum waiting time is {}", transactionManagerIngestionConfig.getPollIntervalSeconds(), transactionManagerIngestionConfig.getTimeWaitSeconds());
                     try {
                         // Multiply by 1000 for millis instead of seconds
-                        sleep(1000L * transactionManagerConfig.getPollIntervalSeconds());
+                        sleep(1000L * transactionManagerIngestionConfig.getPollIntervalSeconds());
                     } catch (InterruptedException e) {
                         log.warn("Thread sleep while waiting for in progress cursor to succeed was interrupted! Swallowing exception and allowing while loop to continue.");
                     }
@@ -119,6 +132,51 @@ public class ExtendTransactionService extends TransactionService {
                     arrangementCursor = getArrangementCursor(arrangementExternalId);
                 }
         });
+
+        if (ORDER_BY_BOOKING_DATE.equals(parameterHolder.getOrderBy()) && transactionManagerAdditionOrderingConfig.isEnabled()) {
+            var pendingTransactions = response.getTransactionItems().stream()
+                    .filter(it -> BILLING_STATUS_UNBILLED.equals(it.getBillingStatus()) || BILLING_STATUS_PENDING.equals(it.getBillingStatus()))
+                    .collect(Collectors.toList());
+
+            var postedTransactions = response.getTransactionItems().stream()
+                    .filter(it -> BILLING_STATUS_BILLED.equals(it.getBillingStatus()))
+                    .collect(Collectors.toList());
+
+            response.getTransactionItems().clear();
+
+            // If no billing statuses were sent as a parameter we want to sort/return all transactions
+            // If it was provided, we only want to sort/return UNBILLED/PENDING if those were requested
+            if (parameterHolder.getBillingStatuses() == null || parameterHolder.getBillingStatuses() != null && parameterHolder.getBillingStatuses().contains(BILLING_STATUS_UNBILLED) || parameterHolder.getBillingStatuses().contains(BILLING_STATUS_PENDING)) {
+                pendingTransactions = pendingTransactions.stream()
+                        .filter(it -> BILLING_STATUS_UNBILLED.equals(it.getBillingStatus()) || BILLING_STATUS_PENDING.equals(it.getBillingStatus()))
+                        .sorted(Comparator.comparing(t -> t.getAdditions().get(transactionManagerAdditionOrderingConfig.getPendingAddition())))
+                        .collect(Collectors.toList());
+
+                if (DIRECTION_DESCENDING.equals(parameterHolder.getDirection())) {
+                    pendingTransactions = pendingTransactions.stream()
+                            .sorted(Comparator.comparing(t -> t.getAdditions().get(transactionManagerAdditionOrderingConfig.getPendingAddition()), Comparator.reverseOrder()))
+                            .collect(Collectors.toList());
+                }
+
+                response.getTransactionItems().addAll(pendingTransactions);
+            }
+
+            if (parameterHolder.getBillingStatuses() == null || parameterHolder.getBillingStatuses() != null && parameterHolder.getBillingStatuses().contains(BILLING_STATUS_BILLED)) {
+                postedTransactions = postedTransactions.stream()
+                        .filter(it -> BILLING_STATUS_BILLED.equals(it.getBillingStatus()))
+                        .sorted(Comparator.comparing(t -> t.getAdditions().get(transactionManagerAdditionOrderingConfig.getPostedAddition())))
+                        .collect(Collectors.toList());
+
+                if (DIRECTION_DESCENDING.equals(parameterHolder.getDirection())) {
+                    postedTransactions = postedTransactions.stream()
+                            .sorted(Comparator.comparing(t -> t.getAdditions().get(transactionManagerAdditionOrderingConfig.getPostedAddition()), Comparator.reverseOrder()))
+                            .collect(Collectors.toList());
+                }
+
+                response.getTransactionItems().addAll(postedTransactions);
+            }
+        }
+
         return response;
     }
 
